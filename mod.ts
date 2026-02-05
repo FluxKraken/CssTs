26a01c3333621b36e4b2d337c456275c96eebc5a
Flux Kraken <fluxkraken@pm.me>
2026-02-05 14:08:15 -0500

    Configure for Publish to JSR

---
+++ mod.ts
@@ -0,0 +1,9 @@
+// @deno-types="./dist/index.d.ts"
+export { default, vite, cv } from "./dist/index.js";
+
+export type {
+  CssTsPluginOptions,
+  StyleDeclaration,
+  StyleSheet,
+  StyleValue,
+} from "./dist/index.d.ts";
